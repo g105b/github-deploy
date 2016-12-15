d966301b8a6d7e842a3c7714d752d2df450c9a0e
Greg Bowler <greg.bowler@g105b.com>
2016-12-15 13:07:14 +0000

    Configurable paths.

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,33 @@ then
 	exit 1
 fi
 
-CFG_TMP_DIR="/tmp/deploy"
-CFG_FILES_DIR="/var/deploy/files"
-# TODO: Load cfg values over the top of these ^.
+CFG_TMP_PATH="/tmp/deploy"
+CFG_DEPLOY_BASE_PATH="/var/deploy"
+CFG_FILES_PATH="/var/deploy/files"
+
+# Overwrite the configurable variables with any set in config.ini .
+if [ -a config.ini ]; then
+	VALUE=$(awk -F "=" '/deploy_tmp_path/ {print $2}' config.ini)
+	if [ -z $VALUE ]; then
+		CFG_TMP_PATH=$VALUE
+	fi
+
+	VALUE=$(awk -F "=" '/deploy_base_path/ {print $2}' config.ini)
+	if [ -z $VALUE ]; then
+		CFG_DEPLOY_BASE_PATH=$VALUE
+	fi
+
+	VALUE=$(awk -F "=" '/deploy_files_path/ {print $2}' config.ini)
+	if [ -z $VALUE ]; then
+		CFG_FILES_PATH=$VALUE
+	fi
+fi
 
 DEPLOY_TYPE_TAG="deploy-type-tag"
 DEPLOY_TYPE_BRANCH="deploy-type-branch"
 DEPLOY_TYPE=""
 DEPLOY_REF=""
-DEPLOY_PATH="/var/deploy/$CIRCLE_PROJECT_REPONAME"
+DEPLOY_PATH="$CFG_DEPLOY_BASE_PATH/$CIRCLE_PROJECT_REPONAME"
 
 case $1 in
 "production")
@@ -47,7 +65,7 @@ $DEPLOY_TYPE_BRANCH)
 esac
 
 # stream to tmp directory first; this avoids downtime during stream.
-TMPDIR=CFG_TMP_DIR
+TMPDIR=CFG_TMP_PATH
 REMOTE_SSH_TAR_COMMAND="rm -rf $TMPDIR; mkdir -p $TMPDIR; cd $TMPDIR; tar xzf -"
 CMD_SSH_TAR="ssh $SSH_CONNECTION '$REMOTE_SSH_TAR_COMMAND'"
 # Circle will execute this script within the repo directory.
@@ -57,7 +75,7 @@ echo "Executing: tar czf - '$CIRCLE_PROJECT_REPONAME/' | eval $CMD_SSH_TAR"
 tar czf - "$CIRCLE_PROJECT_REPONAME/" | eval $CMD_SSH_TAR
 
 # Copy any deployment files over the new deployment.
-DEPLOY_FILES_PATH="$CFG_FILES_DIR/$CIRCLE_PROJECT_REPONAME"
+DEPLOY_FILES_PATH="$CFG_FILES_PATH/$CIRCLE_PROJECT_REPONAME"
 CMD_SSH_FILES="cp -R $DEPLOY_FILES_PATH/* $TMPDIR/$CIRCLE_PROJECT_REPONAME"
 # Make a backup of the old deployment.
 CMD_BACKUP="if [ -d $DEPLOY_PATH ]; then rm -rf $DEPLOY_PATH.old; mv $DEPLOY_PATH $DEPLOY_PATH.old; fi"
